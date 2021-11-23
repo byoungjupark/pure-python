846b9413990bd018c2dcb2c59e49bc09aea288db
byoungjupark <byoungju1012@gmai.com>
2021-11-23 12:32:36 +0900

    Set grpc protobuf

--- validate.py
+++ validate.py
@@ -1,4 +1,8 @@
 import re
+import bcrypt
+
+from database import *
+from models import *
 
 
 def email_validate(e):
@@ -19,3 +23,22 @@ def password_validate(p):
         raise Exception("non valid password")
 
     return True
+
+
+def hash_password(email, password):
+    valid_email = email_validate(email)
+    valid_password = password_validate(password)
+
+    if valid_email and valid_password:
+        hashed_password = bcrypt.hashpw(
+            password.encode("utf-8"), bcrypt.gensalt()
+        ).decode()
+        return hashed_password
+
+
+def is_exist_email(email):
+    with db.get_db() as session:
+        if session.query(User).filter(User.email == email).count() > 0:
+            raise Exception("Existed email already")
+        else:
+            return email
